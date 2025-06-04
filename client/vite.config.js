dac0376adbb64936745148a86d7efe5be9de0281
Sohom Banerjee <74492703+BSohom@users.noreply.github.com>
2025-06-04 20:58:48 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default {
   preview: {
     port: parseInt(process.env.PORT) || 4173,
     host: '0.0.0.0',
-    allowedHosts: ['musica-r9re.onrender.com'], // ✅ Add your Render domain here
+    allowedHosts: ['musica-bsohom-2025.onrender.com'], // ✅ Add your Render domain here
   },
   server: {
     proxy: {
